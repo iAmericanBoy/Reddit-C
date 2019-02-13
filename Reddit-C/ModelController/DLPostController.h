3fbc0040aa02ca4ee504bfb1178e430c9be32550
iAmericanBoy <32912858+iAmericanBoy@users.noreply.github.com>
2019-02-13 14:30:57 -0700

    Implements fetchPostForSearchTerm:completion: and fetchImageForThumpnail:completion:

--- Reddit-C/ModelController/DLPostController.h
+++ Reddit-C/ModelController/DLPostController.h
@@ -8,12 +8,16 @@
 
 #import <Foundation/Foundation.h>
 #import "DLPost.h"
+#import <UIKit/UIKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface DLPostController : NSObject
 
-
++(void)fetchPostForSearchTerm:(NSString *)serchTerm
+                 completion:(void (^_Nullable) (NSMutableArray<DLPost *> *_Nullable card))completion;
++(void)fetchImageForThumpnail:(DLPost *)post
+              completion:(void (^_Nullable) (UIImage *_Nullable pic))completion;
 
 @end
 
--- Reddit-C/ModelController/DLPostController.m
+++ Reddit-C/ModelController/DLPostController.m
@@ -10,4 +10,74 @@
 
 @implementation DLPostController
 
+static NSString * const baseURlString = @"https://www.reddit.com/r/";
+
+
++ (void)fetchPostForSearchTerm:(NSString *)serchTerm completion:(void (^)(NSMutableArray<DLPost *> * _Nullable))completion
+{
+    NSURL *baseURL = [[NSURL alloc]initWithString:baseURlString];
+    
+    NSURL *searchURL = [baseURL URLByAppendingPathComponent:[serchTerm lowercaseString]];
+    
+    NSURL *searchURLWithFileExtension = [searchURL URLByAppendingPathExtension:@"json"];
+    
+    NSLog(@"%@", searchURLWithFileExtension);
+    
+    NSMutableArray<DLPost *> *emptyArray = [NSMutableArray<DLPost *> new];
+    
+    [[[NSURLSession sharedSession] dataTaskWithURL:searchURL completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        if (error) {
+            NSLog(@"There was an Error in %s :%@, %@", __PRETTY_FUNCTION__, error, error.localizedDescription);
+            completion(emptyArray);
+            return ;
+        }
+        if (response) {
+            NSLog(@"%@",response);
+        }
+        
+        if (data) {
+            NSDictionary *topLevelDictionary = [NSJSONSerialization JSONObjectWithData:data options: 0 error:&error];
+            if (!topLevelDictionary) {
+                NSLog(@"error parsing the JSON %@", error);
+                completion(emptyArray);
+                return;
+            }
+            
+            NSArray *childArray = topLevelDictionary[@"data"][@"children"];
+            
+            NSMutableArray<DLPost *> *array = [NSMutableArray<DLPost *> new];
+
+            for (NSDictionary* dicionary in childArray) {
+                DLPost *newPost = [[DLPost alloc] initWithDictionary:dicionary];
+                [array addObject:newPost];
+            }
+            completion(array)
+
+
+        }
+    }] resume];
+}
+
+
++ (void)fetchImageForThumpnail:(DLPost *)post completion:(void (^)(UIImage * _Nullable))completion
+{
+    NSURL *baseURL = [NSURL URLWithString:post.thumbnailURLAsString];
+    NSLog(@"%@",baseURL);
+    
+    [[[NSURLSession sharedSession] dataTaskWithURL:baseURL completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        if (error) {
+            NSLog(@"There was an Error in %s :%@, %@", __PRETTY_FUNCTION__, error, error.localizedDescription);
+            completion(nil);
+            return ;
+        }
+        if (response) {
+            NSLog(@"%@",response);
+        }
+        
+        if (data) {
+            UIImage *cardpic = [[UIImage alloc] initWithData:data scale:1];
+            completion(cardpic);
+        }
+    }] resume];
+}
 @end
