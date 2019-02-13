b91dd2d84f18228f1a6e79d51e7b81abeb6bf206
iAmericanBoy <32912858+iAmericanBoy@users.noreply.github.com>
2019-02-13 13:15:42 -0700

    Initial Commit

---
+++ Reddit-C/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Reddit-C
+//
+//  Created by Dominic Lanzillotta on 2/13/19.
+//  Copyright © 2019 Dominic Lanzillotta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
